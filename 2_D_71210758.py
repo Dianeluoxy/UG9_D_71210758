70246c31d9501f6ae5d1be715803d86806545772
luoxy <dianeluoxy.zabdi@ti.ukdw.ac.id>
2021-11-11 20:01:54 +0700

    Jawaban soal 2 UG9

---
+++ 2_D_71210758.py
@@ -0,0 +1,6 @@
+anak1 = int(input("Uang saku anak pertama : "))
+anak2 = int(input("Uang saku anak kedua : "))
+anak3 = int(input("Uang saku anak ketiga : "))
+
+mean = ((anak1+anak2+anak3)/3)
+print("Rata-rata dari",str(anak1)+"+"+str(anak2)+"+"+str(anak3),"adalah", mean)
