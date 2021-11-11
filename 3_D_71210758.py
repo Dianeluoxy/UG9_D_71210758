3941b5a316aa24ce8b7545abd0c5c7bbee7d342b
luoxy <dianeluoxy.zabdi@ti.ukdw.ac.id>
2021-11-11 20:02:36 +0700

    Jawaban soal 3 UG9

---
+++ 3_D_71210758.py
@@ -0,0 +1,7 @@
+barang1 = input("Masukkan daftar belanja 1: ")
+barang2 = input("Masukkan daftar belanja 2: ")
+barang3 = input("Tambahkan data ke daftar belanja 1: ")
+barang4 = input("Tambahkan data ke daftar belanja 2: ")
+
+print("Daftar belanja 1 adalah", barang1+","+barang3)
+print("Daftar belanja 2 adalah", barang2+","+barang4)
